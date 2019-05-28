282dc1daef510d2eda999c57b367b0bc4a7abbfa
John O'Rourke <officialjohnorourke@gmail.com>
2019-05-28 12:35:05 -0400

    Finishes palindrome module

--- lib/johnoro_palindrome.rb
+++ lib/johnoro_palindrome.rb
@@ -1,6 +1,24 @@
 require "johnoro_palindrome/version"
 
 module JohnoroPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+  def palindrome?
+    if processed_content.empty?
+      false
+    else
+      processed_content == processed_content.reverse
+    end
+  end
+
+  private
+    def processed_content
+      self.to_s.scan(/[a-z0-9]/i).join.downcase
+    end
+end
+
+class String
+  include JohnoroPalindrome
+end
+
+class Integer
+  include JohnoroPalindrome
 end
